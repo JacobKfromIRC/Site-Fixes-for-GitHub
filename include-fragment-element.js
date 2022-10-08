f08b2db48a8a71051af405d3d97b9c9ee680c304
Jacob K <112278581+JacobKfromIRC@users.noreply.github.com>
2022-10-07 19:45:44 -0500

    Support lazy loading using (an old version of) the include-fragment-element by GitHub. The script included is a modified version of v5.1.2, because v5.1.3 and up use TypeScript and I don't currently have a good way to deal with that.

---
+++ include-fragment-element.js
@@ -0,0 +1,170 @@
+// Copyright (c) 2014-2018 GitHub, Inc.
+//
+// SPDX-License-Identifier: LicenseRef-MIT-include-fragment-element
+
+/* Copied from
+	https://github.com/github/include-fragment-element/releases/tag/v5.1.2 */
+
+/* Currently using v5.1.2 instead of the latest version because versions after
+	5.1.2 uses typescript and I don't have a good way to deal with that
+	currently. Maybe after Haketilo roadmap #16 is finished or Trisquel updates
+	the node-typescript package to allow compiling newer TypeScript, we can
+	update to the latest version of this library. */
+
+/* Modified by Jacob K in 2022 to remove the "export default" keywords so the
+	script can work in Haketilo. */
+
+const privateData = new WeakMap()
+
+function fire(name, target) {
+  setTimeout(function() {
+    target.dispatchEvent(new Event(name))
+  }, 0)
+}
+
+function handleData(el) {
+  return getData(el).then(
+    function(html) {
+      const parentNode = el.parentNode
+      if (parentNode) {
+        el.insertAdjacentHTML('afterend', html)
+        parentNode.removeChild(el)
+      }
+    },
+    function() {
+      el.classList.add('is-error')
+    }
+  )
+}
+
+function getData(el) {
+  const src = el.src
+  let data = privateData.get(el)
+  if (data && data.src === src) {
+    return data.data
+  } else {
+    if (src) {
+      data = el.load()
+    } else {
+      data = Promise.reject(new Error('missing src'))
+    }
+    privateData.set(el, {src, data})
+    return data
+  }
+}
+
+function isWildcard(accept) {
+  return accept && !!accept.split(',').find(x => x.match(/^\s*\*\/\*/))
+}
+
+/*export default */class IncludeFragmentElement extends HTMLElement {
+  constructor() {
+    super()
+  }
+
+  static get observedAttributes() {
+    return ['src']
+  }
+
+  get src() {
+    const src = this.getAttribute('src')
+    if (src) {
+      const link = this.ownerDocument.createElement('a')
+      link.href = src
+      return link.href
+    } else {
+      return ''
+    }
+  }
+
+  set src(val) {
+    this.setAttribute('src', val)
+  }
+
+  get accept() {
+    return this.getAttribute('accept')
+  }
+
+  set accept(val) {
+    this.setAttribute('accept', val)
+  }
+
+  get data() {
+    return getData(this)
+  }
+
+  attributeChangedCallback(attribute) {
+    if (attribute === 'src') {
+      // Source changed after attached so replace element.
+      if (this._attached) {
+        handleData(this)
+      }
+    }
+  }
+
+  connectedCallback() {
+    this._attached = true
+    if (this.src) {
+      handleData(this)
+    }
+  }
+
+  disconnectedCallback() {
+    this._attached = false
+  }
+
+  request() {
+    const src = this.src
+    if (!src) {
+      throw new Error('missing src')
+    }
+
+    return new Request(src, {
+      method: 'GET',
+      credentials: 'same-origin',
+      headers: {
+        Accept: this.accept || 'text/html'
+      }
+    })
+  }
+
+  load() {
+    return Promise.resolve()
+      .then(() => {
+        fire('loadstart', this)
+        return this.fetch(this.request())
+      })
+      .then(response => {
+        if (response.status !== 200) {
+          throw new Error(`Failed to load resource: the server responded with a status of ${response.status}`)
+        }
+        const ct = response.headers.get('Content-Type')
+        if (!isWildcard(this.accept) && (!ct || !ct.includes(this.accept ? this.accept : 'text/html'))) {
+          throw new Error(`Failed to load resource: expected ${this.accept || 'text/html'} but was ${ct}`)
+        }
+        return response
+      })
+      .then(response => response.text())
+      .then(
+        data => {
+          fire('load', this)
+          fire('loadend', this)
+          return data
+        },
+        error => {
+          fire('error', this)
+          fire('loadend', this)
+          throw error
+        }
+      )
+  }
+
+  fetch(request) {
+    return fetch(request)
+  }
+}
+
+if (!window.customElements.get('include-fragment')) {
+  window.IncludeFragmentElement = IncludeFragmentElement
+  window.customElements.define('include-fragment', IncludeFragmentElement)
+}
